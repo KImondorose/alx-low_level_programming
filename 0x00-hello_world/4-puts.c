a39338c7a6a23675ac61ac5febd7c3d61f23aaf1
KImondorose <rosewairimu1@gmail.com>
2022-09-08 10:35:36 +0300

    Corrected

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-    /* Write a C program that prints exactly "Programming is like building a
-    multilingual puzzle, followed by a new line.
-    *Use the function puts
-    *You are not allowed to use printf
-    *Your program should end with the value 0
-    */
+/* Write a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line. *Use the function puts
+ *You are not allowed to use printf
+ *Your program should end with the value 0
+*/
 
 int main(void)
 {
