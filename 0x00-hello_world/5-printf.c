08dde9957f5cb28be4f2c8bf9ca24ac2e62c2c7d
KImondorose <rosewairimu1@gmail.com>
2022-09-08 11:50:04 +0300

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,6 @@
 /**
  * main - prints the phrase with proper grammar, but the outcome is a piece of art,
  * Return: 0 if exited
- * program should compile without warning when using the -Wall gcc option
  */
 int main(void)
 {
