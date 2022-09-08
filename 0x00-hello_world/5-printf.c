eb7b32d44258db8a7a194816099d00147833712d
KImondorose <rosewairimu1@gmail.com>
2022-09-08 10:52:10 +0300

    five

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints the phrase with proper grammar, but the outcome is a piece of art,
+ * Return: 0 if exited
+ * program should compile without warning when using the -Wall gcc option
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
