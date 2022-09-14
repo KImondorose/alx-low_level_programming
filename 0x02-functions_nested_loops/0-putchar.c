02608aa9c17241fe78388999e354fee72f44242c
KImondorose <rosewairimu1@gmail.com>
2022-09-14 19:08:48 +0300

    Print alphabet

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,20 +2,17 @@
 #include "main.h"
 
 /**
- * main- print the alphabet in lowercase followed by a new line
- * Return: 0 (success)
+ * print_alphabet- print the alphabet in lowercase followed by a new line
+ * Return: nothing
  */
-int main(void)
-{
-char print_alphabet(void);
+
+void print_alphabet(void)
 {
 	int i;
 
 	for (i = 97; i <= 122; i++)
-	{
-		putchar(i);
-	}
-}
+    {
+        putchar(i);
 	putchar('\n');
-    return('0');
+    }
 }
