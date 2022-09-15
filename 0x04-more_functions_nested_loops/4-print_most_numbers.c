e1b1b2254b0db9d1d54ca983a121f88184fc4fd1
Rose Kimondo <63718507+KImondorose@users.noreply.github.com>
2022-09-15 23:22:09 +0300

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,23 +1,19 @@
 #include "main.h"
+
 /**
- * print_most_numbers - print numbers
- *
- * Return: Always 0.
+ * print_most_numbers - prints number
  */
+
 void print_most_numbers(void)
 {
+	int i;
 
-int i;
-
-i = 0;
-while (i < 10)
-{
-if (i != 2 && i != 4)
-{
-_putchar (i + '0');
+	for (i = 48; i < 58; i++)
+	{
+		if (i != 50 && i != 52)
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
 }
-i++;
-}
-
-_putchar ('\n');
-}	
