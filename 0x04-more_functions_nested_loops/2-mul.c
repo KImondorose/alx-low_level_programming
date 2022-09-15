b698193ec24c4eeb4c9714ffa5763b82086ada62
Rose Kimondo <63718507+KImondorose@users.noreply.github.com>
2022-09-15 22:58:53 +0300

    Update 2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,25 +1,12 @@
 #include "main.h"
-
 /**
- * Write a function that multiplies two integers.
- * Prototype: int mul(int a, int b);
- * Return: 0
- */ 
+ * mul -multiplies two integers
+ *@a: print int
+ *@b: print int
+ * Return: Always 0.
+ */
 
-int main (void)
+int mul(int a, int b)
 {
-    int a;
-    int b;
-    int mult;
-
-    printf("Enter the first number\n");
-    scanf("%d", &a);
-
-    printf("ENter the second number\n");
-    scanf("%d", &b);
-
-    mult = a*b;
-
-    printf("THe product of %d and %d is %d\n", a, b, mult);
-    return (0);
+return (a * b);
 }
