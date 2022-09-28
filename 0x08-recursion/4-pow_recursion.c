ddc4f8adb9274dcfcf38f82d7178ddae33b0dfcf
Rose Kimondo <63718507+KImondorose@users.noreply.github.com>
2022-09-28 12:08:13 +0300

    Update 4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+
 /**
  * _pow_recursion - returns the value of x raised to the power of y
  * @x: value to raise
@@ -14,4 +15,4 @@ int _pow_recursion(int x, int y)
 	if (y == 0)
 		return (1);
 	return (x * _pow_recursion(x, y - 1));
-
+}
